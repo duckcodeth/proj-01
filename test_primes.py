370193a6db2b41bcefc09ba2910c27dc06f62474
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2014-09-07 10:47:01 +0200

    ttt

--- test_primes.py
+++ test_primes.py
@@ -8,6 +8,14 @@ class PrimesTestCase(unittest.TestCase):
         """Is five successfully determined to be prime?"""
         self.assertTrue(is_prime(5))
 
+    def test_is_four_non_prime(self):
+    	"""Is four correctly determined not to be prime?"""
+    	self.assertFalse(is_prime(4), msg='Four is not prime!')
+
+    def test_is_zero_not_prime(self):
+        """Is zero correctly determined not to be prime?"""
+        self.assertFalse(is_prime(0))
+
 if __name__ == '__main__':
     unittest.main()
     
